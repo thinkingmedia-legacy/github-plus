e44f6bb29d5ad91a8beb023d99b24fd8df112e19
thinkingmedia <mathew@thinkingmedia.ca>
2014-02-09 17:56:45 -0500

    problems with the new issue button.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+GitHubPlus = {
+
+	templates: [],
+
+	/**
+	 * Loads a template
+	 *
+	 * @param {string} name
+	 * @param {function} fn Call back when the template is loaded.
+	 */
+	template: function(name,fn) {
+		if(GitHubPlus.template[name] === undefined)
+		{
+			$.get(chrome.extension.getURL('templates/'+name+'.mustache')).done(function(tmpl){
+				fn(tmpl);
+			});
+		}
+		else
+		{
+			fn(GitHubPlus.template[name]);
+		}
+	},
+
+	button: function(title) {
+	}
+};
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -28,13 +55,35 @@ chrome.extension.sendMessage({}, function(response) {
 		$("a.minibutton.sidebar-button[title='Download this repository as a zip file']").addClass("primary");
 
 		// set the logo
-		$.get(chrome.extension.getURL('templates/logo.mustache')).done(function(tmpl){
+		GitHubPlus.template('logo',function(tmpl){
 			var data = {
 				url: chrome.extension.getURL('icons/icon24.png')
 			};
 			$("div.header:first").append(Mustache.render(tmpl,data));
 			$(".ext-github-plus-logo").tipsy({gravity: 'w'});
 		});
+
+		// viewing a repo
+		if(path.match(/^\/.+\/.+$/))
+		{
+			GitHubPlus.template('button',function(tmpl){
+				var data = {
+					class: "new-issue",
+					url: path+"/issues/new",
+					title: 'New Issue'
+				};
+				var file = $(".file-navigation:first");
+				if(file.length != 0)
+				{
+					return;
+				}
+				var btn = $(Mustache.render(tmpl,data)).appendTo("body");
+				var p = file.offset();
+				var w = file.outerWidth() - btn.outerWidth();
+				btn.css('top', p.top+3+'px');
+				btn.css('left', p.left+w+'px');
+			});
+		}
 	}
 	}, 10);
 });
